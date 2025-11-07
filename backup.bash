c2dfe1edc387f99ebc31ec693bdedc55ba6d4f83
AkELkA <akelka.dds@gmail.com>
2025-11-07 23:26:52 +0300

    Update backup.bash

--- backup.bash
+++ backup.bash
@@ -8,7 +8,7 @@ set -e
 # --- CONFIGURATION ---
 
 # Set this to your mod's folder name (as it appears in BG3's project directories)
-MOD_SUBDIR_NAME="DGB_Body_AkELkA_533a435e-8c13-bc21-7cfc-48c3a322e29a"
+MOD_SUBDIR_NAME="Dragonborn_children_Umi_Doni_efd8ecc1-35f0-fa73-186f-f8a1b71af763"
 
 # Path to the Baldur’s Gate 3 Data directory (can be adjusted if needed)
 BG3_DATA="E:/El/Games/Steam/steamapps/common/Baldurs Gate 3/Data"
